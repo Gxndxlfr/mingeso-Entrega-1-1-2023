428c83ad88b8d55a518dc605afb6603e2b620c22
Israel Guzman <israel.guzman@usach.cl>
2023-03-24 14:32:15 -0300

    Primera entidades y application files

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("01537", "QUISPE S. ALCIDES", "A");
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("02994", "BELLO R. PHILIP", "B");
+INSERT INTO proveedores(codigo,nombre,categoria,retencion) VALUES ("013312", "ROMAN A. PABLO", "C");
